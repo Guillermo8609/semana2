17342e662ebf7350629ebfa67933a83ac79081dd
Guillermo8609 <guillermosandoval_86@hotmail.com>
2020-07-20 06:10:29 -0500

    Finalizado

---
+++ js/funciones.js
@@ -0,0 +1,9 @@
+
+function suma(){
+var nume1=parseInt(document.getElementById("num1").value);
+var nume2=parseInt(document.getElementById("num2").value);
+var resultados
+resultados= nume1+nume2
+alert("El reslutado es " + resultados)
+
+}
