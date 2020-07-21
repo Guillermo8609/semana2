3f54ed4b9248467b88e5c7200149c2c1c41c264f
Guillermo8609 <guillermosandoval_86@hotmail.com>
2020-07-21 04:50:25 -0500

    Lo intenté de nuevo

---
+++ Actividad Diaria D7/JavaScript/funciones.js
@@ -0,0 +1,16 @@
+function abrirPestaña(){
+    var panelBlock = document.getElementsByName("panel");
+    for (var i = 0; i < panelBlock.length; i++){
+        panelBlock[i].style.display = "none";
+    }
+    
+
+
+}
+
+
+
+// var panelElts = document.getElementsByClassName("panel");
+// for( var i = 0; i < panelElts.length; i++){
+// console.log(panelElts[i])
+// } 
